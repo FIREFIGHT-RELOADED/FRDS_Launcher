535b38d7855b9d21fe3048bacfe050993c5a6a69
Bitl <toycrazy@hotmail.com>
2017-11-23 11:14:02 -0700

    initial

---
+++ FRDS_Launcher/FRDS_Launcher/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace FRDS_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(297, 21);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(108, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "FIREFIGHT RELOADED Server";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Player Limit";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(393, 108);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(393, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Launch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(218, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "27015";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Server Port";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(236, 60);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(169, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Enable UPNP Port Forwarding";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(186, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Maps";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 265);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FIREFIGHT RELOADED Server Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FRDS_Launcher/FRDS_Launcher/Form1.cs
@@ -0,0 +1,111 @@
+﻿using Open.Nat;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FRDS_Launcher
+{
+    public partial class Form1 : Form
+    {
+        public static string BasePath = AppDomain.CurrentDomain.BaseDirectory;
+        public static string ModPath = BasePath + "firefightreloaded/";
+        public static string EXEPath = BasePath + "fr.exe";
+        public static string MapPath = ModPath + "maps/";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public async void StartUPNP()
+        {
+           var nat = new NatDiscoverer();
+
+           var cts = new CancellationTokenSource(5000);
+           var device = await nat.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+           await device.CreatePortMapAsync(new Mapping(Protocol.Udp, Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox2.Text), "FIREFIGHT RELOADED"));
+
+           var ip = await device.GetExternalIPAsync();
+        }
+
+        public async void StopUPNP()
+        {
+            var nat = new NatDiscoverer();
+            var cts = new CancellationTokenSource(5000);
+            var device = await nat.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+
+            foreach (var mapping in await device.GetAllMappingsAsync())
+            {
+                if (mapping.Description.Contains("FIREFIGHT RELOADED"))
+                {
+                    await device.DeletePortMapAsync(mapping);
+                }
+            }
+
+            var ip = await device.GetExternalIPAsync();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                StartUPNP();
+            }
+
+            string args = "-console -game firefightreloaded -steam -textmode -port " + textBox2.Text + " +maxplayers " + numericUpDown1.Value.ToString() + " +map " + listBox1.SelectedItem.ToString() + " +hostname " + textBox1.Text;
+
+            Process client = new Process();
+            client.StartInfo.FileName = EXEPath;
+            client.StartInfo.Arguments = args;
+            client.EnableRaisingEvents = true;
+            client.Exited += new EventHandler(ServerExited);
+            client.Start();
+        }
+
+        void ServerExited(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                StopUPNP();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string mapdir = MapPath;
+            DirectoryInfo dinfo = new DirectoryInfo(mapdir);
+            FileInfo[] Files = dinfo.GetFiles("*.bsp");
+            foreach (FileInfo file in Files)
+            {
+                listBox1.Items.Add(file.Name);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (checkBox1.Checked == true)
+            {
+                StopUPNP();
+            }
+
+            Process[] fr = Process.GetProcessesByName("fr");
+            if (fr != null)
+            {
+                foreach (var process in fr)
+                {
+                    process.Kill();
+                }
+            }
+        }
+    }
+}
